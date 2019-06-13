f2ca7c06cae12bdce901f80918fb03a261cc6e8f
jonny phun <jonny_phun@hotmail.com>
2019-06-13 23:45:23 +0000

    enrollments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 
 
   has_many :courses
+  has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
 
   
   # Include default devise modules. Others available are:
@@ -11,4 +13,13 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  def enrolled_in?(course)
+    
+    
+    return enrolled_courses.include?(course)
+
+  end
+
 end
