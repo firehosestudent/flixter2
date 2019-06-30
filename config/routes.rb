cd7a7aa38067b219f8395291fbbb8250ea880acd
jonny phun <jonny_phun@hotmail.com>
2019-06-30 03:31:53 +0000

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resource :dashboard, only: [:show] 
+
   root 'static_pages#index'
 
   resources :courses, only: [:index, :show] do
